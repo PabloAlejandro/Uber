6a6a17d9ae2c0d006ad8040f3dd8c53a4a9fd064
Pablo A. Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-07-23 16:48:13 +0100

    ImageDownloader helper class added
    This class downloads images async for the collection view calls the
    delegate when finished

---
+++ Utils/ImageDownloader/ImageDownloader.m
@@ -0,0 +1,37 @@
+//
+//  ImageDownloader.m
+//  Uber
+//
+//  Created by Pau (Personal) on 23/07/2016.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import "ImageDownloader.h"
+#import "NetworkFactoryRequests.h"
+
+@implementation ImageDownloader
+
+- (void)downloadImageForUrl:(NSURL *)url collectionView:(UICollectionView *)collectionView indexPath:(NSIndexPath *)indexPath
+{
+    NetworkFactoryRequests * factoryRequest = [[NetworkFactoryRequests alloc] init];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [factoryRequest sendAsynchronousRequest:request completionHandler:^(NSURLResponse * response, NSData * data, NSError * error) {
+        if(error == nil) {
+            UIImage * image = [[UIImage alloc] initWithData:data];
+            if (image) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if([self.delegate respondsToSelector:@selector(imageDidDownload:collectionView:forIndexPath:)])
+                        [self.delegate imageDidDownload:image collectionView:collectionView forIndexPath:indexPath];
+                });
+            }
+        }
+        else {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if([self.delegate respondsToSelector:@selector(imageDidFailDownload:collectionView:forIndexPath:)])
+                    [self.delegate imageDidFailDownload:error collectionView:collectionView forIndexPath:indexPath];
+            });
+        }
+    }];
+}
+
+@end

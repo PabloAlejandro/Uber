1c1712d9a5fafdc5b88f04434ee0cbf5f15012dd
Pablo A. Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-07-23 16:19:51 +0100

    Project files

---
+++ Uber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Uber
+//
+//  Created by Pau (Personal) on 23/07/2016.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

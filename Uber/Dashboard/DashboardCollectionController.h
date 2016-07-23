9930f04470aa58c78baf3b0a7d2b80e3a4ee3ba6
Pablo A. Perez Martinez <pablo.alejandro.pm@gmail.com>
2016-07-23 16:34:20 +0100

    Collection controller and custom cell added
    Collection controller is the responsible for managing the collection
    view and implement its delegates and data source

---
+++ Uber/Dashboard/DashboardCollectionController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardCollectionController.h
+//  Uber
+//
+//  Created by Pau (Personal) on 23/07/2016.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardCollectionController : UIViewController
+
+@end
---
+++ Uber/Dashboard/PhotoCollectionCell.m
@@ -0,0 +1,21 @@
+//
+//  PhotoCollectionCell.m
+//  Uber
+//
+//  Created by Pau (Personal) on 23/07/2016.
+//  Copyright © 2016 Uber. All rights reserved.
+//
+
+#import "PhotoCollectionCell.h"
+
+@implementation PhotoCollectionCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
